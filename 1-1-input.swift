30a4d913a572eccf83007609340b23738c54a784
Wen-Tien Chang <ihower@gmail.com>
2016-02-22 01:39:20 +0800

    Add 2 swift examples

---
+++ 1-1-input.swift
@@ -0,0 +1,5 @@
+print("What's your name:")
+
+var name = readLine()!
+
+print("Hello, " + name)
---
+++ 1-2-calculate.swift
@@ -0,0 +1,11 @@
+print("Height: ")
+
+var height = Float( readLine()! )!
+
+print("Weight: ")
+
+var weight = Float(  readLine()! )!
+
+var area = height * weight / 2
+
+print("面積: " + String(area)  )
